462a85817ffafe8368435a35a71da3300ddfa6f8
Jake Lehle <84940857+JakeLehle@users.noreply.github.com>
2022-06-03 13:40:40 -0500

    Update and rename instagram_non_followers.py to Instagram_non_followers.py

--- Instagram_non_followers.py
+++ Instagram_non_followers.py
@@ -3,6 +3,11 @@ from instabot import Bot
 import config
 import requests
 import re
+import time
+from random import seed
+from random import randint
+
+
 #Set up the bot object
 bot = Bot()
 #Login and request cookies from session and save them
@@ -13,12 +18,22 @@ raw_username_list = []
 #Convert userID to username
 for x in not_following:
     print(x)
+
+
+seed(1)
 for x in not_following:
     x = str(x)
     url = ['https://www.instagram.com/graphql/query/?query_hash=c9100bf9110dd6361671f113dd02e7d6&variables={\"user_id\":\"', x, '\",\"include_chaining\":false,\"include_reel\":true,\"include_suggested_users\":false,\"include_logged_out_extras\":false,\"include_highlight_reels\":false,\"include_related_profiles\":false}']
     url_joined = ''.join(url)
+    str(url_joined)
+    print(url_joined)
     url_output = requests.get(url_joined)
+    print(url_output.text)
     raw_username_list.append(url_output.text)
+    value = randint(5, 45)
+    time.sleep(value)
+
+
 #Filter string
 print(raw_username_list)
 joined_username_string = "".join(raw_username_list)
